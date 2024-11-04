e999f30f1ad06b4f7719ef65385d4544e19287b9
deuojen <qnurix@hotmail.com>
2024-11-04 12:03:32 +0000

    feat: complete all task
    update node

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,85 @@
 'use strict';
 
 module.exports = function (app) {
+  const stock_likes_ip = {};
+  app.route('/api/stock-prices').get(async function (req, res) {
+    // https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/[symbol]/quote
+    // https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/goog/quote
+    // console.log(req.query);
+    // { stock: 'GOOG', like: 'true' }
+    // { stock: [ 'GOOG', 'MSFT' ], like: 'true' }
 
-  app.route('/api/stock-prices')
-    .get(function (req, res){
-      
+    let stocks = convertStringToArray(req.query.stock);
+    let like = req.query.like === 'true' ? true : false;
+    let ip_addr = req.ip;
+    //let isIpExists = stock_likes_ip.indexOf(ip_addr) > -1;
+
+    let stock1 = {
+      stock: '',
+      price: 0,
+    };
+
+    let stock2 = {
+      stock: '',
+      price: 0,
+    };
+
+    let stockname = stocks[0].toLowerCase();
+    //console.log(stock_likes_ip[stockname]);
+    if (!stock_likes_ip[`${stockname}`]) {
+      stock_likes_ip[`${stockname}`] = [];
+    }
+
+    let url = `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stockname}/quote`;
+    const response = await fetch(url);
+    const data = await response.json();
+    // console.log(data);
+    stock1.stock = data.symbol;
+    stock1.price = data.latestPrice;
+
+    if (stocks.length == 2) {
+      let stockname2 = stocks[1].toLowerCase();
+      //console.log(stock_likes_ip[stockname]);
+      if (!stock_likes_ip[`${stockname2}`]) {
+        stock_likes_ip[`${stockname2}`] = [];
+      }
+      let url = `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stockname2}/quote`;
+      let response = await fetch(url);
+      let data = await response.json();
+      // console.log(data);
+      stock2.stock = data.symbol;
+      stock2.price = data.latestPrice;
+
+      // console.log(stock_likes_ip);
+
+      let like_count1 = stock_likes_ip[`${stockname}`].length;
+      let like_count2 = stock_likes_ip[`${stockname2}`].length;
+      stock1['rel_likes'] = -like_count1;
+      stock2['rel_likes'] = like_count2;
+
+      return res.json({
+        stockData: [stock1, stock2],
+      });
+    }
+
+    if (like) {
+      if (stock_likes_ip[`${stockname}`].indexOf(ip_addr) < 0) {
+        stock_likes_ip[`${stockname}`].push(ip_addr);
+      }
+    }
+
+    let like_count = stock_likes_ip[`${stockname}`].length;
+    stock1['likes'] = like_count;
+
+    return res.json({
+      stockData: stock1,
     });
-    
+
+    // {"stockData":{"stock":"GOOG","price":786.90,"likes":1}}
+    // {"stockData":[{"stock":"MSFT","price":62.30,"rel_likes":-1},{"stock":"GOOG","price":786.90,"rel_likes":1}]}
+  });
+
+  const convertStringToArray = (object) => {
+    return typeof object === 'string' ? Array(object) : object;
+  };
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -5,6 +5,69 @@ const server = require('../server');
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
-
+suite('Functional Tests', function () {
+  // #1
+  test('Viewing one stock: GET request to /api/stock-prices/', function (done) {
+    chai
+      .request(server)
+      .get('/api/stock-prices?stock=GOOG')
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.property(res.body['stockData'], 'stock', 'GOOG');
+        done();
+      });
+  });
+  // #2
+  test('Viewing one stock and liking it: GET request to /api/stock-prices/', function (done) {
+    chai
+      .request(server)
+      .get('/api/stock-prices?stock=GOOG&like=true')
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.property(res.body['stockData'], 'stock', 'GOOG');
+        assert.property(res.body['stockData'], 'likes', 1);
+        done();
+      });
+  });
+  // #3
+  test('Viewing the same stock and liking it again: GET request to /api/stock-prices/', function (done) {
+    chai
+      .request(server)
+      .get('/api/stock-prices?stock=GOOG&like=true')
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.property(res.body['stockData'], 'stock', 'GOOG');
+        assert.property(res.body['stockData'], 'likes', 1);
+        done();
+      });
+  });
+  // #4
+  test('Viewing two stocks: GET request to /api/stock-prices/', function (done) {
+    chai
+      .request(server)
+      .get('/api/stock-prices?stock=GOOG&stock=MSFT')
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.property(res.body['stockData'][0], 'stock', 'GOOG');
+        assert.property(res.body['stockData'][1], 'stock', 'MSFT');
+        done();
+      });
+  });
+  // #5
+  test('Viewing two stocks and liking them: GET request to /api/stock-prices/', function (done) {
+    chai
+      .request(server)
+      .get('/api/stock-prices?stock=GOOG&stock=MSFT&like=true')
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.property(res.body['stockData'][0], 'stock', 'GOOG');
+        assert.property(res.body['stockData'][1], 'stock', 'MSFT');
+        assert.hasAllKeys(res.body['stockData'][0], [
+          'stock',
+          'price',
+          'rel_likes',
+        ]);
+        done();
+      });
+  });
 });
